c0718982e27f6233fe68e04433b2ee832ad208f3
nostam <matsonho@gmail.com>
2021-02-26 11:48:41 +0100

    add user schema

---
+++ src/services/users/schema.js
@@ -0,0 +1,56 @@
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      rquired: [true, "Username required"],
+      minlength: [3, "Username is too short"],
+      validate: {
+        async function(username) {
+          const user = await this.constructor.findOne({ username });
+          if (user && user.username === this.username) return true;
+          return !user ? true : false;
+        },
+        message: "Username is taken",
+      },
+    },
+    favourites: [{ city: String, country: String }],
+    googleId: String,
+    refreshToken: [{ token: { type: String } }],
+  },
+  { timestamps: true }
+);
+
+UserSchema.methods.toJSON = function () {
+  const user = this;
+  const userObject = user.toObject();
+  delete userObject.password;
+  delete userObject.__v;
+  return userObject;
+};
+
+UserSchema.statics.findByCredentials = async function (username, password) {
+  const user = await this.findOne({ username });
+  if (user) {
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (isMatch) return user;
+    else return null;
+  } else {
+    return null;
+  }
+};
+
+UserSchema.pre("save", async function (next) {
+  const user = this;
+  const plainPW = user.password;
+
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(plainPW, 10);
+  }
+  next();
+});
+
+module.exports = model("user", UserSchema);
